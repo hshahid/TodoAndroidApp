f389e6becd6e497c9b6cd88b8d2eabc5f3db9343
Hashma <shahi22h@mtholyoke.edu>
2016-06-21 17:16:22 +0500

    “update”

--- app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hashma.todoapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -46,10 +47,10 @@ public class MainActivity extends AppCompatActivity {
 //        );
 //    }
 
-//    public void onAddItem(View v) {
-//        etNewItem = (EditText)findViewById(R.id.etNewItem);
-//        String itemText = etNewItem.getText().toString();
-//        itemsAdapter.add(itemText);
-//        etNewItem.setText("");
-//    }
+    public void onAddItem(View v) {
+        etNewItem = (EditText)findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
 }
