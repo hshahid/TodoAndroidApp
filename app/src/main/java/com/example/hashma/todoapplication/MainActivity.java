43e5e9ca6c35ee5e2d664ddd57ba578c22cceb9f
Hashma <shahi22h@mtholyoke.edu>
2016-06-21 16:49:26 +0500

    “update”

--- app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
@@ -6,16 +6,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvItems;
     private EditText etNewItem;
     private Button btnAddItem;
+    private List<String> items;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
