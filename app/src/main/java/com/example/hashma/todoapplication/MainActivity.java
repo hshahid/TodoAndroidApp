5ba4a6bd73c61999d8447cd6f85bf33381cf2566
Hashma <shahi22h@mtholyoke.edu>
2016-06-21 16:37:21 +0500

    “update”

--- app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.hashma.todoapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvItems;
-
+    private EditText etNewItem;
 
 
     @Override
