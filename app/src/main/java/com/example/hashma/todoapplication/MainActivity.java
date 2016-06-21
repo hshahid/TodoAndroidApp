e003c95c2130d2157a6a12260e357a29bcb20ad1
Hashma <shahi22h@mtholyoke.edu>
2016-06-21 16:52:41 +0500

    “update”

--- app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
+++ app/src/main/java/com/example/hashma/todoapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         items = new ArrayList<String>();
-        itemsAdapter = new ArrayAdapter<String>();
+        //itemsAdapter = new ArrayAdapter<String>(this, R.layout.simple_list)
 
     }
 }
